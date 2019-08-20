3f76127e287cf1e1168c088e447210fda9e4f547
Arun Menon <menonarun1988@gmail.com>
2019-08-20 12:02:37 +0530

    update our menuitems to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
+import {Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage.component';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+
+)
+
+
 function App() {
   return (
     <div>
-     <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+
+
     </div>
   );
 }
